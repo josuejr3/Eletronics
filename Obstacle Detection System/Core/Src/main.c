365dd9244b0bcd990bb1cb199b579618f0001f35
Josue <geenujunior@gmail.com>
2025-12-31 04:30:40 -0300

    add: systemclock_config

---
+++ Obstacle Detection System/Core/Src/main.c
@@ -0,0 +1,67 @@
+#include "main.h"
+
+static void SystemClock_Config(void);
+
+int main(void){
+
+  HAL_Init();
+  SystemClock_Config();
+
+  while (1)
+  {
+
+  }
+
+}
+
+void Error_Handler(void)
+{
+    __disable_irq();   // Desliga interrupções
+
+    while (1)
+    {}
+}
+
+static void SystemClock_Config(void){
+
+  // Evita lixo de memória nas structs
+  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+
+  // Tratmento do PWR e Voltage Scaling
+  __HAL_RCC_PWR_CLK_ENABLE();                                       // Habilita o Clock da Interface de Energia
+  __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE1);    // Regulador de Tensão no Modo 2
+
+  RCC_OscInitStruct.OscillatorType      = RCC_OSCILLATORTYPE_HSI;
+  RCC_OscInitStruct.HSIState            = RCC_HSI_ON;
+  RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
+  RCC_OscInitStruct.PLL.PLLState        = RCC_PLL_ON;
+  RCC_OscInitStruct.PLL.PLLSource       = RCC_PLLSOURCE_HSI;
+
+  RCC_OscInitStruct.PLL.PLLM  = 8;                                  // << depende do cristal
+  RCC_OscInitStruct.PLL.PLLN  = 168;
+  RCC_OscInitStruct.PLL.PLLP  = RCC_PLLP_DIV2;
+  RCC_OscInitStruct.PLL.PLLQ  = 4;
+
+  // Aplica configurações no oscilador
+  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  // Configuração dos barramentos e clock do sistema
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_SYSCLK | RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2;
+  RCC_ClkInitStruct.SYSCLKSource   = RCC_SYSCLKSOURCE_PLLCLK;
+  RCC_ClkInitStruct.AHBCLKDivider  = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV4;
+  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV2;
+
+  // Aplica configurações do clock 
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_5) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+}
+
+// https://chatgpt.com/share/6954b373-d268-8008-a737-1e3ec2b70576 - Como ativar IntelliSense STM32 VSCode
